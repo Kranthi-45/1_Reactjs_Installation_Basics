a0006e5a3a405084d0a822adc69d2d205ad61705
Kranthi-45 <kranthikumarbolli@gmail.com>
2024-02-18 11:09:20 +0530

    added detail description for reactjs basics

--- react-proj-demo/like_button.js
+++ react-proj-demo/like_button.js
@@ -1,7 +1,10 @@
 'use strict';
 
 const e = React.createElement;
+
+// class component   -  traditional reactjs , need to extend to React.component to convert class to class component
 class LikeButton extends React.Component {
+
   constructor(props) {
     super(props);
     this.state = { liked: false };
@@ -10,30 +13,38 @@ class LikeButton extends React.Component {
     if (this.state.liked) {
       return 'You liked this.';
     }
-    
-    return e(
-      'button',
-      { onClick: () => this.setState({ liked: true }) },
-      'Like'
-    );
 
-// JSX syntax    
- return (
-    <button onClick={() => this.setState({ liked: true }) }>
-      Like
-    </button>
- );
+// writing HTML code in .js fle - using tradition syntax React.creatElement as we cannot write html code direclty in .js files 
+// React.createElement("tagname","attr","text")
+
+    // return React.createElement(
+    //   'button',
+    //   { onClick: () => this.setState({ liked: true }) },
+    //   'Like'
+    // );
+
+// writing HTML code in .js fle - using JSX syntax
+// enclose html code in braces ( )
+
+    return (
+      <button onClick={() => this.setState({ liked: true })}>
+        Like
+      </button>
+    );
 
   }
 }
 
-//->>>>>>>>>>>>>> ReactDOM.createRoot().render();
-const domContainer = document.querySelector('#like_button_container');
-// const domContainer = document.getElementById('#like_button_container');
-const root = ReactDOM.createRoot(domContainer);
-root.render(e(LikeButton));
+//->>>>>>>>>>>>>>Rendering using ReactDOM.createRoot().render();
 
-// -->>>>>>> JSX simple render
+// const domContainer = document.querySelector('#like_button_container');
+// const domContainer = document.getElementById('like_button_container');
+// const root = ReactDOM.createRoot(domContainer);
+// root.render(e(LikeButton));
+
+//->>>>>>>>>>>>>> Renderging use JSX simple render
+
 // let domContainer = document.querySelector('#like_button_container');
-// ReactDOM.render(<LikeButton />, domContainer);
+let domContainer = document.getElementById('root');
+ReactDOM.render(<LikeButton />, domContainer);
 
